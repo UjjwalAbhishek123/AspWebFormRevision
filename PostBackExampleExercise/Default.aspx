f857c8a9599bc6a96201d81f8f33f9e4f07abf19
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2026-04-04 14:34:35 +0530

    PostBack Exercise for practice

---
+++ PostBackExampleExercise/Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PostBackExampleExercise.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1 {
+            width: 100%;
+        }
+
+        .style2 {
+            width: 332%;
+        }
+
+        .style3 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>League of Greatest Singers
+            <asp:Label ID="lblWelcome" runat="server"></asp:Label>
+        </h1>
+
+        <table class="style1">
+            <tr>
+                <td class="style2">Click below to choose a player...
+                    <asp:Label ID="lblName" runat="server"></asp:Label>
+                </td>
+                <td class="style3" rowspan="2">
+                    <asp:Image ID="img0" runat="server" Height="150px" Width="150px" ImageUrl="~/images/MusicGroup.jpg" />
+                </td>
+            </tr>
+            <tr>
+                <td class="style2">
+                    <asp:DropDownList ID="ddlSingerName" runat="server" OnSelectedIndexChanged="DdlSingerName_Display" AutoPostBack="true">
+                        <asp:ListItem Value="0" Selected="True">Select Singers</asp:ListItem>
+                        <asp:ListItem Value="1">Mohammad Rafi</asp:ListItem>
+                        <asp:ListItem Value="2">Lata Mangeshkar</asp:ListItem>
+                        <asp:ListItem Value="3">Kishore Kumar</asp:ListItem>
+                        <asp:ListItem Value="4">Sonu Nigam</asp:ListItem>
+                        <asp:ListItem Value="5">Shreya Ghoshal</asp:ListItem>
+                        <asp:ListItem Value="6">Arijit Singh</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+    </form>
+
+</body>
+</html>
---
+++ PostBackExampleExercise/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostBackExampleExercise
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblWelcome.Text = "Welcome";
+                //img0.ImageUrl = "images/MusicGroup.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex != 0)
+            {
+                lblWelcome.Text = "Team Legends";
+            }
+        }
+
+        protected void DdlSingerName_Display(object sender, EventArgs e)
+        {
+            if (ddlSingerName.SelectedIndex == 1)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Mohammad Rafi.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex == 2)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Lata Mangeshkar.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex == 3)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Kishore Kumar.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex == 4)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Sonu Nigam.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex == 5)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Shreya Ghoshal.jpg";
+            }
+            else if (ddlSingerName.SelectedIndex == 6)
+            {
+                lblName.Text = ddlSingerName.SelectedItem.Text;
+                img0.ImageUrl = "images/Arijit Singh.jpg";
+            }
+        }
+    }
+}

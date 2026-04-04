d71f2970366ce4798fadceeb0de125967cf6f52f
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2026-04-04 20:52:47 +0530

    Navigation demo using query strings and session concept.

---
+++ NavigationExample2_QueryString/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NavigationExample2_QueryString
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //navigation using Query string => for example http://abcd.com/abcd.aspx?username=abcd&password=abcd1, here "?username=abcd&password=abcd1" is query string
+
+            string QueryString = "";
+            QueryString = "?ID=" + txtLogin.Text; //here we are passing the value of txtLogin to query string with name "ID"
+            Response.Redirect("Page2.aspx" + QueryString);
+        }
+
+        protected void btnLogin2_Click(object sender, EventArgs e)
+        {
+            //passing data without showing in query string, we can use "Session"
+            Session["userId"] = txtLogin2.Text; //here we are passing the value of txtLogin2 to session with name "userId"
+
+            Response.Redirect("Page2.aspx");
+        }
+    }
+}
---
+++ NavigationExample2_QueryString/Page2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NavigationExample2_QueryString
+{
+    public partial class Page2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Query String check
+                if (Request.QueryString["ID"] != null)
+                {
+                    lblResult.Text = "Welcome " + Request.QueryString["ID"]; //here we are getting the value of query string with name "ID" and displaying it in lblResult
+                }
+                else
+                {
+                    lblResult.Text = "Welcome Guest (no query string)"; //if there is no query string with name "ID" then this message will be displayed in lblResult
+                }
+
+                //Session check
+                if (Session["userId"] != null)
+                {
+                    //getting value in session will not be shown in query string, and we can get it using session name "userId"
+                    lblResult2.Text = Session["userId"].ToString(); //here we are getting the value of session with name "userId" and displaying it in lblResult2
+                }
+                else
+                {
+                    lblResult2.Text = "No session value found";
+                }
+            }
+        }
+    }
+}
